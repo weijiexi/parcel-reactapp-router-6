2874cc48c5af6b698f5dee324ec88da718006c41
Weijie XI <wxi76@berkeley.edu>
2024-09-20 11:16:11 -0700

    setup router 6 - Link

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
-import { Routes, Route,} from "react-router-dom";
+import { Routes, Route, Link} from "react-router-dom";
 import { Home } from "./Home";
 
 export function App() {
 
-    return (<>
-
+    return (
+        <div className='App'>
+            <nav>
+                <Link to="/home">Home</Link>
+            </nav>
             <Routes>
                 <Route path="/home" element={<Home/>}/>
             </Routes>
-        </>
+        </div>
     );
 }
