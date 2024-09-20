578d647c39c55bc6bce1c2c2b75fbd9c130c0163
Weijie XI <wxi76@berkeley.edu>
2024-09-20 11:26:52 -0700

    setup components ItemsList

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,20 @@
 import { Routes, Route, Link} from "react-router-dom";
 import { Home } from "./Home";
+import  { ItemsList } from "./ItemsList";
 
 export function App() {
 
     return (
         <div className='App'>
             <nav>
+                <h1 className='store-header'>Emily&apos;s Trinkets</h1>
                 <Link to="/home">Home</Link>
             </nav>
+
             <Routes>
-                <Route path="/home" element={<Home/>}/>
+                <Route path="/items-list" element={<ItemsList />}/>
             </Routes>
+
         </div>
     );
 }
---
+++ src/components/ItemsList.js
@@ -0,0 +1,9 @@
+export const ItemsList = props => {
+
+    return (
+        <div className='items-list-wrapper'>
+            <h1> This is Items List Page</h1>
+        </div>
+    )
+
+}
